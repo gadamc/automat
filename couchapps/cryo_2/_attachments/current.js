cccd028c453c7e99dfe9864cd8fb371e9fd5094b
cox <cox@e591df6e-0647-4dd9-86e1-2d6e1b804871>
2011-06-19 09:36:45 +0000

    initial commit to svn
    git-svn-id: https://edwdev-ik.fzk.de/svn/couchdb/automat/trunk@87 e591df6e-0647-4dd9-86e1-2d6e1b804871

---
+++ couchapps/cryo_2/_attachments/current.js
@@ -0,0 +1,76 @@
+var db = $.couch.db(window.location.pathname.split("/")[1]);
+
+var cryoVal=['T_Bolo', 'T_Speer', 'T_PT1', 'T_B1K', 'P_B1K',
+                'P_injection', 'debit', 'I_Alim1', 'I_Alim2',
+                'I_Alim3', 'I_Alim4', 'GM_P', 'PT_P', 'Pt_1',
+                'Pt_2', 'Pt_3', 'Hc1', 'Hc2', 'Hc3', 'Hc4', 'Co_France',
+                'Co_Italie', 'Ba_grotte', 'Ba_nemo']; 
+
+$(document).ready(function() {
+    fillData();
+});
+
+function fillData()
+{
+    db.view("data/byipaddress",  {
+        key:"134.158.176.112",
+        reduce:false,
+        descending:true,
+        include_docs:true,
+        limit:1,
+        success:function(data){ 
+            
+            var doc = data.rows[0].doc;
+
+            if (doc){
+                
+                addToStats(doc);
+            }
+            else{
+                displayError();
+            }
+         },
+         error: function(req, textStatus, errorThrown){alert('Error '+ textStatus);}
+         
+    });
+}
+
+function addToStats(doc){
+    var p = document.getElementById("title");
+    var today = new Date(doc.utctime*1000.0)
+    p.innerHTML = "<h1>Latest Readings from the Cryostat</h1><br>";
+    p.innerHTML += today.toUTCString();
+    p.innerHTML += "<br><br>";
+    
+    var tbl = document.getElementById('data');
+	var tblbody = tbl.getElementsByTagName("tbody");
+	//clear the table in case its allready full of elements
+	for(var i = 0; i < tblbody.length; i++){tbl.removeChild(tblbody[i]);}
+	
+	var tblbody = document.createElement("tbody");
+	
+    for (var i = 0; i <  cryoVal.length; i++) {
+
+        if (doc.hasOwnProperty(cryoVal[i])) {
+            var row = document.createElement("tr");
+                
+            var cell = document.createElement("td");
+            cell.appendChild(document.createTextNode( cryoVal[i] ));
+            row.appendChild(cell);
+                
+            var cell = document.createElement("td");
+            cell.appendChild(document.createTextNode( doc[cryoVal[i]] ));
+            row.appendChild(cell);
+                
+            tblbody.appendChild(row);
+        }
+    }
+    tbl.appendChild(tblbody);
+    
+}
+
+function displayError(){
+    var p = document.getElementById("title");
+    p.innerHTML = "<h1>Failed to get document from the database.</h1>";
+
+}
---
+++ getdata.py
@@ -0,0 +1,304 @@
+#!/usr/bin/env python
+
+import socket, re, sys, copy, datetime, time, math
+from couchdbkit import Server, Database
+
+
+def buildDoc(ip, port):
+    doc = dict()
+    doc['author'] = 'Adam Cox'
+    doc['type'] = 'automat_data'
+    dd = datetime.datetime.utcnow()
+    doc['ipaddr'] = ip
+    doc['port'] = port
+    doc['date'] = {'year':dd.year, 'month':dd.month, 'day':dd.day, 'hour':dd.hour, \
+        'minute':dd.minute, 'second':dd.second, 'microsecond':dd.microsecond} 
+    doc['utctime'] = time.time()
+    doc['_id'] = 'automat_data_' + ip + '_' + str(doc['utctime'])
+
+    return doc
+
+
+#_______________
+# format value
+def formatvalue(value):
+    if (isinstance(value,str)):
+        # #see if this string is really an int or a float
+        if value.isdigit()==True: #int
+            return int(value)
+        else: #try a float
+            try:
+                if math.isnan(float(value))==False:
+                    return float(value)
+            except:
+                pass
+        
+        return value.strip('" ') #strip off any quotations and extra spaces found in the value
+    else:
+        return value
+
+
+
+#_____________
+# upload
+def upload(db, docs):
+    print 'upload', len(docs), 'docs at', datetime.datetime.now()
+    db.bulk_save(docs)
+    del docs
+    return list()
+
+
+def getValues():
+    
+    global s
+    global completeRec
+    global startValues
+    
+    prevn = []
+    stillLooking = True
+    foundStartConfig = False;
+    
+    valuelist = list()
+    flaglist = list()
+    indexlist = list()
+    while stillLooking:
+        
+        data = s.recv(4096)
+        newn = data.split('\n')
+        
+        if completeRec.match(newn[0]) == None and len(prevn) > 0:
+            newn[0] = prevn[len(prevn)-1] + newn[0]
+        
+        for i in range(len(newn)):
+            if startValues.match(newn[i]):
+                
+                for j in range(i+1, len(newn)):    
+                    if startRec.match(newn[i]) == False:
+                        stillLooking = False
+                        break
+                    else:
+                        if re.search(';', newn[j]):
+                            valuelist.append( formatvalue(newn[j].split(';')[2]) )
+                            flaglist.append( formatvalue(newn[j].split(';')[1]) )
+                #print newn[j]
+                
+                while stillLooking:
+                    prevn = copy.copy(newn)
+                    data = s.recv(4096)
+                    newn  = data.split('\n')
+                    
+                    if completeRec.match(newn[0]) == None and len(prevn) > 0:
+                        newn[0] = prevn[len(prevn)-1] + newn[0]
+                    
+                    for ii in range(len(newn)):
+                        if startRec.match(newn[ii]) == None:
+                            if re.search(';', newn[ii]):
+                                valuelist.append( formatvalue(newn[ii].split(';')[2]) )
+                                flaglist.append( formatvalue(newn[ii].split(';')[1]) )
+                        #print newn[ii]
+                        else:
+                            stillLooking = False
+                            break
+                
+                break
+        
+        
+        prevn = copy.copy(newn)
+    
+    
+    return [valuelist, flaglist]
+
+
+
+def getConfig():
+    
+    global s
+    global completeRec
+    global startValues
+    global startConfig
+    global startRec
+    
+    prevn = []
+    stillLooking = True
+    foundStartConfig = False;
+    
+    namelist = list()
+    typelist = list()
+    maxWait = 20
+    count = 0
+    s.send('2;2\r\n')
+    print '...waiting for configuration'
+    while stillLooking:
+        if count > maxWait: #this is needed in case we missed the results from the command, so we force the loop to send the command again
+            s.send('2;2\r\n')
+            print '...waiting for configuration'
+            count = 0
+        count += 1
+        data = s.recv(4096)
+        newn = data.split('\n')
+        
+        if completeRec.match(newn[0]) == None and len(prevn) > 0:
+            newn[0] = prevn[len(prevn)-1] + newn[0]
+        
+        for i in range(len(newn)):
+            if startConfig.match(newn[i]):
+                #print newn[i]
+                
+                for j in range(i+1, len(newn)):    
+                    if startRec.match(newn[i]) == False:
+                        stillLooking = False
+                        break
+                    else:
+                        if re.search(';', newn[j]):
+                            namelist.append( formatvalue(newn[j].split(';')[0]) )
+                            typelist.append( formatvalue(newn[j].split(';')[1]) )
+                #print newn[j]
+                
+                while stillLooking:
+                    prevn = copy.copy(newn)
+                    data = s.recv(4096)
+                    newn  = data.split('\n')
+                    
+                    if completeRec.match(newn[0]) == None and len(prevn) > 0:
+                        newn[0] = prevn[len(prevn)-1] + newn[0]
+                    
+                    for ii in range(len(newn)):
+                        if startRec.match(newn[ii]) == None:
+                            if re.search(';', newn[ii]):
+                                namelist.append( formatvalue(newn[ii].split(';')[0]) )
+                                typelist.append( formatvalue(newn[ii].split(';')[1]) )
+                        #print newn[ii]
+                        else:
+                            stillLooking = False
+                            break
+                
+                break
+        
+        
+        prevn = copy.copy(newn)
+    
+    
+    return [namelist, typelist]
+
+
+
+def main(*args):
+    '''
+    This is a very hacky script that reads data from an Edelweiss Automat server and places
+    the results on a couch database.
+    there are 5 arguments needed to run this script
+
+        automatip = args[0].   The IP address of the Edelweiss Automat server
+        port = int(args[1])    The port that the Automat server is using
+        couchS = args[2]       The URI of the couchdb server ('http://127.0.0.1:5984')
+        dbname = args[3]       The database name on the couchdb server
+        sleepttime = args[4]   The amount of time this script will pause between data acquisition (could be useful to slow down the amount of data stored, if necessary)
+    
+    '''
+    if len(args) != 5:
+        print 'need five arguments'
+        help(main)
+        return
+    
+    automatip = args[0]
+    port = int(args[1])
+    couchS = args[2]
+    dbname = args[3]
+    sleeptime = int(args[4])
+
+    print automatip, port, couchS, dbname, sleeptime
+
+    global s
+    global startRec
+    global startValues
+    global startConfig
+    global startStates
+    global startController
+    global startClients
+    global completeRec
+    startRec = re.compile('[$]{2};\w+;\.*')
+    startValues = re.compile('[$]{2};0;.*')
+    startConfig = re.compile('[$]{2};1;.*')
+    startStates = re.compile('[$]{2};2;.*')
+    startController = re.compile('[$]{2};3;.*')
+    startClients = re.compile('[$]{2};4;.*')
+    completeRec = re.compile('\w+;\w+;\w+')
+    
+    serv = Server(couchS)
+    db = serv[dbname]
+    print db.info()
+            
+    try:    
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((automatip, port))
+    
+    except Exception as e:
+        print e
+        print 'socket exception... will wait one minute'
+        doc = buildDoc(automatip, port)
+        subdoc = {}
+        subdoc['type'] = type(e)
+        subdoc['args'] = e.args
+        doc['exception'] = subdoc
+        db.save_doc(doc)
+        
+        time.sleep(60.0)
+        main(*args)
+        
+        
+    ret =  getConfig()
+    print 'configuration found'
+    namelist = ret[0]
+    typelist = ret[1]
+
+    
+    checkpoint = 4
+    docs = list()
+
+    try:
+        while True:
+            
+            ret = getValues()
+            
+            flaglist = ret[1]
+            valuelist = ret[0]
+            
+            doc = buildDoc(automatip, port)
+            
+            for i in range(len(namelist)):
+                ''' this is too much data..? we just need the values really. 
+                    
+                d = list()
+                if i < len(typelist):
+                    d.append(typelist[i])
+                if i < len(flaglist):
+                    d.append(flaglist[i])
+                if i < len(valuelist):
+                    d.append(valuelist[i])
+                '''
+                if i < len(valuelist):
+                    doc[namelist[i]] = valuelist[i]
+                        
+
+            docs.append(doc)    
+
+            if len(docs)%checkpoint==0:
+                docs = upload(db,docs)
+                
+            if sleeptime != 0:
+                s.close()
+                time.sleep(sleeptime)  #sleep...
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.connect((automatip, port))
+
+    except Exception as inst:
+        print type(inst)     # the exception instance
+        print inst.args      # arguments stored in .args
+        print inst           # __str__ allows args to printed directly
+
+    #finish uploading any docs. 
+    docs = upload(db,docs)
+
+
+if __name__ == '__main__':
+    main(*sys.argv[1:])
